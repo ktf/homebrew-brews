7e2226d2858f809ed3fc210f1239d109cb5bfb7b
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-09-13 14:52:29 +0200

    More fixes

--- cern-get-sso-cookie.rb
+++ cern-get-sso-cookie.rb
@@ -1,10 +1,12 @@
 class CernGetSsoCookie < Formula
   desc "Tool to get authentication cookie from CERN SSO"
   homepage ""
+  url "https://github.com/ktf/cern-get-sso-cookie.git", :using => :git
   head "https://github.com/ktf/cern-get-sso-cookie.git", :using => :git
 
   def install
-    system "cp -r usr/bin", "#{prefix}/bin"
-    system "cp -r usr/share", "#{prefix}/share"
+    system "mkdir -p #{prefix}"
+    system "cp -r cern-get-sso-cookie/usr/bin", "#{prefix}/bin"
+    system "cp -r cern-get-sso-cookie/usr/share", "#{prefix}/share"
   end
 end
