328c24a528f9be3e9c7610c95c9dc6254790625f
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-09-13 14:54:58 +0200

    More typos

--- cern-get-sso-cookie.rb
+++ cern-get-sso-cookie.rb
@@ -5,8 +5,8 @@ class CernGetSsoCookie < Formula
   head "https://github.com/ktf/cern-get-sso-cookie.git", :using => :git
 
   def install
-    system "mkdir -p #{prefix}"
-    system "cp -r cern-get-sso-cookie/usr/bin", "#{prefix}/bin"
-    system "cp -r cern-get-sso-cookie/usr/share", "#{prefix}/share"
+    system "mkdir", "-p", #{prefix}"
+    system "cp", "-r", "cern-get-sso-cookie/usr/bin", "#{prefix}/bin"
+    system "cp", "-r", "cern-get-sso-cookie/usr/share", "#{prefix}/share"
   end
 end
