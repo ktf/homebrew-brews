1f8fb66ca4cadd19a840b873398df7e5bf3d1551
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-05-15 15:21:58 +0200

    Make sure we can download a private repo

--- alice-proxy-tools.rb
+++ alice-proxy-tools.rb
@@ -3,13 +3,13 @@ require_relative "lib/custom_download_strategy"
 class AliceProxyTools < Formula
   desc "Tool to setup ALICE SOCKS proxies"
   homepage ""
-  url "https://github.com/ktf/alice-proxy-tools.git", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  head "https://github.com/ktf/alice-proxy-tools.git", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/ktf/alice-proxy-tools", :using => :github_private_repo
+  head "https://github.com/ktf/alice-proxy-tools/tarball/master", :using => :github_private_repo
   version "1.0"
 
   def install
     system "mkdir -p #{prefix}/bin"
-    system "cp -r alice-proxy-tools/alice-setup-proxy #{prefix}/bin"
-    system "cp -r alice-proxy-tools/alice-dispose-proxy #{prefix}/bin"
+    system "cp -r alice-setup-proxy #{prefix}/bin"
+    system "cp -r alice-dispose-proxy #{prefix}/bin"
   end
 end
